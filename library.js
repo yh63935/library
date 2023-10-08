6567ba8b36b5741ad75a5de7ead0348d40693ef0
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2023-10-07 17:06:27 -0700

    Add new book from form data to library:

--- library.js
+++ library.js
@@ -1,58 +1,76 @@
-const myLibrary = [];
-const bookAuthor = prompt("Book author?")
-const bookTitle = prompt("Book title?")
-const bookPages = 36
-const bookRead = "Yes"
-const bookRating = 10
-const bookTable = document.querySelector('table')
+let myLibrary = [];
+const bookTable = document.querySelector('table');
+const dialog = document.querySelector('dialog');
+const tableBody = document.querySelector('tbody');
 
-myLibrary.push({ author: "c", title: "g", pages: 36, read: "Yes", rating: 10 }) 
-myLibrary.push({ author: "h", title: "g", pages: 36, read: "Yes", rating: 10 }) 
-
-function Book(author, title, pages, read, rating) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
-    this.rating = rating;
-    return author, title, pages, read, rating;
+function Book(arr) {
+    this.author = arr[0].value;
+    this.title = arr[1].value;
+    this.pages = arr[2].value;
+    this.read = arr[3].value;
+    this.rating = arr[4].value;
 }
 
 function addBookToLibrary() {
-    const newBook = new Book(bookAuthor, bookTitle, bookPages, bookRead, bookRating);
-    myLibrary.push(newBook);
+    const submit = document.querySelector('input[type="submit"]');
+    submit.addEventListener('click',(e)=> {
+        changed = true;
+        e.preventDefault();
+        let allInputs = Array.from(document.querySelectorAll('input'));
+        const newBook = new Book(allInputs);
+        myLibrary.push(newBook);
+
+        displayBook();
+    })
 }
 
 function setTableHeader() {
-    const dummyBook = new Book("", "", "", "", "");
+    // const dummyBook = new Book("", "", "", "", "");
+    const tableHeader = document.querySelector('theader')
+    const headerRow = document.createElement('tr');
+    const dummyBook = new Book(["", "", "", "", ""]);
     for(let key in dummyBook) {
         const headerEl = document.createElement('th');
         headerEl.innerText = key;
-        bookTable.append(headerEl);
+        headerRow.appendChild(headerEl)
     }
+    tableHeader.appendChild(headerRow);
 }
 
 function clearTable() {
-    const tableBody = document.querySelector('tbody');
+    myLibrary = [];
     tableBody.innerHTML = "";
 }
 
 const clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', clearTable);
 
+const newBook = document.querySelector('.new-book')
+
+newBook.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+const closeForm = document.querySelector('.close-form');
+
+closeForm.addEventListener('click', () => {
+    dialog.close();
+})
+
+// When newBookk is clicked, form should pop up to take in details
+// Things to think about, do you want to create form in HTML or create when writing JS?
+
 setTableHeader();
 
 // Display each book in the table
 function displayBook() {
     for(let book of myLibrary) {
-        console.log(book)
         let bookRow = bookTable.insertRow();
         for (let prop in book) {
             let bookData = bookRow.insertCell();
             bookData.append(book[prop]);
-            console.log(book[prop]);
+            tableBody.appendChild(bookRow);
         }
     }
 }
 addBookToLibrary();
-displayBook();
